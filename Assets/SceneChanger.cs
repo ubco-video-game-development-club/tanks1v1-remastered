a53df6d07bdc8dfdc25fb0f2f2c8af3b42b91b1a
caffeine98 <49334026+caffeine98@users.noreply.github.com>
2020-07-11 20:36:53 -0700

    Added main menu (incomplete)
    Main menu added. Will be updated at some point in the future

---
+++ Assets/SceneChanger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void LoadMainGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
