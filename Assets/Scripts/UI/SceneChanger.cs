d489c3166a11390ac258184246a845bfcaf725b6
caffeine98 <49334026+caffeine98@users.noreply.github.com>
2020-07-13 03:24:59 -0700

    Added Instructions menu
    Instructions menu added

--- Assets/Scripts/UI/SceneChanger.cs
+++ Assets/Scripts/UI/SceneChanger.cs
@@ -5,11 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
+    [SerializeField] private GameObject mainMenuInstructionsUI;
+
     public void LoadMainGame() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame() {
         Application.Quit();
+    }
+
+    public void Instructions() {
+        mainMenuInstructionsUI.SetActive(true);
+    }
+
+    public void ClickToClose() {
+        //Click on the instruction panel to close it
+
+        mainMenuInstructionsUI.SetActive(false);
     }
 }
