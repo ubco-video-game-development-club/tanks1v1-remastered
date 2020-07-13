f34cfb92f40adb90aae4d0ad1d01f74de330ea89
caffeine98 <49334026+caffeine98@users.noreply.github.com>
2020-07-13 02:38:08 -0700

    Added Pause Menu
    Added Pause Menu. Also corrected the colours on the main menu buttons.

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    [SerializeField] private KeyCode pauseGameKey = KeyCode.Escape;  // maybe a second pause key for the second player?
+
+    void Update()
+    {
+        if (Input.GetKeyDown(pauseGameKey)) {
+
+            if (isPaused) {
+                ResumeGame();
+            }
+
+            else {
+                PauseGame();
+            }
+
+        }
+    }
+
+    public void ResumeGame() {
+        //Resume Game Steps:
+        // 1) Close Pause Menu
+        // 2) Start/Continue Gameplay (Usually by setting timescale to 1)
+
+        isPaused = false;
+        Time.timeScale = 1f;
+        pauseMenuUI.SetActive(false);
+    }
+
+    private void PauseGame() {
+        //Pause Game Steps:
+        // 1) Pause Gameplay (Usually by setting timescale to 0)
+        // 2) Open Pause Menu
+
+        isPaused = true;
+        Time.timeScale = 0f;
+        pauseMenuUI.SetActive(true);
+    }
+
+    public void RestartGame() {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void BackToMainMenu() {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+}
